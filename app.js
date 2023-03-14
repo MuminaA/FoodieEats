fb6baafc2725f8e749b4246779453da4cab9d9a2
mumina <muminaabdi13@gmail.com>
2023-03-14 04:45:49 -0400

    added modal and navbar transition started

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const modal = document.querySelector("#modal");
-const openModal = document.querySelector(".open-button");
-const closeModal = document.querySelector(".close-button");
+// modal effect
 
-openModal.addEventListener("click", () => {
-  modal.showModal();
+let modal = document.querySelector(".modal");
+let open = document.querySelector(".open-button");
+let close = document.querySelector(".close-button");
+let overlay = document.querySelector(".overlay");
+let body = document.querySelector("body");
+
+open.addEventListener("click", function() {
+    modal.classList.add("unhidden");
+    modal.style.display = "flex";
+    body.style.overflowY = "hidden";
 });
 
-closeModal.addEventListener("click", () => {
-  modal.closeModal();
-});
+close.addEventListener("click", function() {
+    modal.classList.remove("unhidden");
+    modal.style.display = "none";
+    body.style.overflowY = "scroll";
+})
+
+// scroll effect
+
+document.onreadystatechange = function() {
+  let lastScrollPosition = 0;
+  const navbar = document.querySelector('.navbar');
+  window.addEventListener('scroll', function() {
+    lastScrollPosition = window.scrollY;
+    
+    if (lastScrollPosition > 100)
+      navbar.classList.add('navbar-white');
+    else
+      navbar.classList.remove('navbar-white');
+  });
+}
